b21d18a06b32798f02133efa9ee33987b3fa47e3
bevin1984 <bevin1984@gmail.com>
2013-05-04 11:19:21 +0800

    创建项目

---
+++ calculator/calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by ken on 13-5-4.
+//  Copyright (c) 2013年 maimaicha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
